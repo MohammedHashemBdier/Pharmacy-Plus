dfb1d4603b554288d44c1040f5e6d9e4c29c16f0
Khaledtello <khaldtllw@gmail.com>
2023-11-21 14:52:23 +0300

    edite medicines table

--- lib/views/widgets/medicines_table.dart
+++ lib/views/widgets/medicines_table.dart
@@ -9,40 +9,65 @@ class MedicinesTable extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetBuilder<MedicineController>(
       init: MedicineController(),
-      builder: (controller) => Column(children: [
-        Row(
-          children: [
-            MyCell('the scientific name'.tr),
-            MyCell('trade name'.tr),
-            MyCell('category'.tr),
-            MyCell('the manufacture company'.tr),
-            MyCell('quantity'.tr),
-            MyCell('expiration date'.tr),
-            MyCell('the price'.tr),
-          ],
-        ),
-        ...controller.filteredMedicines
-            .map((medicine) => Row(
-                  children: [
-                    MyCell(medicine.scientificName!),
-                    MyCell(medicine.commercialName!),
-                    MyCell(medicine.category!),
-                    MyCell(medicine.manufacturer!),
-                    MyCell(medicine.quantity!.toString()),
-                    MyCell(medicine.expiryDate!.toString()),
-                    MyCell(medicine.price!.toString()),
-                    IconButton(
-                      onPressed: () {},
-                      icon: const Icon(Icons.edit),
+      builder: (controller) => DataTable(
+        columnSpacing: 0,
+        columns: [
+          DataColumn(
+            label: Expanded(child: MyCell('the scientific name'.tr)),
+          ),
+          DataColumn(
+            label: Expanded(child: MyCell('trade name'.tr)),
+          ),
+          DataColumn(
+            label: Expanded(child: MyCell('category'.tr)),
+          ),
+          DataColumn(
+            label: Expanded(child: MyCell('the manufacture company'.tr)),
+          ),
+          DataColumn(
+            label: Expanded(child: MyCell('quantity'.tr)),
+          ),
+          DataColumn(
+            label: Expanded(child: MyCell('expiration date'.tr)),
+          ),
+          DataColumn(
+            label: Expanded(child: MyCell('the price'.tr)),
+          ),
+          DataColumn(
+            label: Expanded(child: MyCell('edit'.tr)),
+          ),
+          DataColumn(
+            label: Expanded(child: MyCell('delete'.tr)),
+          ),
+        ],
+        rows: controller.filteredMedicines
+            .map((medicine) => DataRow(cells: [
+                  DataCell(MyCell(medicine.scientificName!)),
+                  DataCell(MyCell(medicine.commercialName!)),
+                  DataCell(MyCell(medicine.category!)),
+                  DataCell(MyCell(medicine.manufacturer!)),
+                  DataCell(MyCell(medicine.quantity!.toString())),
+                  DataCell(MyCell(medicine.expiryDate!.toString())),
+                  DataCell(MyCell(medicine.price!.toString())),
+                  DataCell(
+                    Center(
+                      child: IconButton(
+                        onPressed: () {},
+                        icon: const Icon(Icons.edit),
+                      ),
                     ),
-                    IconButton(
-                      onPressed: () {},
-                      icon: const Icon(Icons.delete),
+                  ),
+                  DataCell(
+                    Center(
+                      child: IconButton(
+                        onPressed: () {},
+                        icon: const Icon(Icons.delete),
+                      ),
                     ),
-                  ],
-                ))
+                  ),
+                ]))
             .toList(),
-      ]),
+      ),
     );
   }
 }
@@ -54,17 +79,12 @@ class MyCell extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: Container(
-        height: 50,
-        alignment: Alignment.center,
-        margin: const EdgeInsets.symmetric(horizontal: 10),
-        child: FittedBox(
-          child: Text(
-            text,
-            style: const TextStyle(fontSize: 30),
-          ),
-        ),
+    return Container(
+      height: 50,
+      alignment: Alignment.center,
+      margin: const EdgeInsets.symmetric(horizontal: 10),
+      child: FittedBox(
+        child: Text(text),
       ),
     );
   }
