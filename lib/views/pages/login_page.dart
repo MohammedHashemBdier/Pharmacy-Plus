67cf3c7fce43061b9791e3badd0c890d3e96e48b
Mohammed Hashem Bdier <mhb20033@gmail.com>
2023-11-23 08:16:48 +0200

    edit some of  Alignment in log in page

--- lib/views/pages/login_page.dart
+++ lib/views/pages/login_page.dart
@@ -18,38 +18,35 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      extendBodyBehindAppBar: true,
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0,
-        leading: Tooltip(
-          message: 'change the language'.tr,
-          child: IconButton(
-            onPressed: () {
-              // تغيير اللغة عند الضغط على زر تغيير اللغة
-              if (Get.locale == const Locale('en')) {
-                Get.updateLocale(const Locale('ar'));
-              } else {
-                Get.updateLocale(const Locale('en'));
-              }
-            },
-            icon: const Icon(
-              Icons.language_sharp,
-              color: Colors.black,
-              size: 35,
-            ),
-          ),
-        ),
-      ),
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.symmetric(
             horizontal: 50,
-            vertical: 50,
           ),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
+              Container(
+                alignment: Alignment.topRight,
+                child: Tooltip(
+                  message: 'change the language'.tr,
+                  child: IconButton(
+                    onPressed: () {
+                      // تغيير اللغة عند الضغط على زر تغيير اللغة
+                      if (Get.locale == const Locale('en')) {
+                        Get.updateLocale(const Locale('ar'));
+                      } else {
+                        Get.updateLocale(const Locale('en'));
+                      }
+                    },
+                    icon: const Icon(
+                      Icons.language_sharp,
+                      color: Colors.black,
+                      size: 35,
+                    ),
+                  ),
+                ),
+              ),
               Image.asset(
                 AppImages.imagesLogo,
                 width: 300,
@@ -73,6 +70,31 @@ class _LoginPageState extends State<LoginPage> {
                   style: const TextStyle(fontSize: 18.0),
                 ),
               ),
+              TextField(
+                decoration: InputDecoration(
+                  border: const OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                    borderSide: BorderSide(),
+                  ),
+                  focusedBorder: const OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                    borderSide: BorderSide(
+                      color: AppColors.c2,
+                    ),
+                  ),
+                  errorBorder: const OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                    borderSide: BorderSide(
+                      color: Colors.red,
+                    ),
+                  ),
+                  labelText: 'username'.tr,
+                  hintText: 'enter the username'.tr,
+                  errorText:
+                      isPasswordIncorrect ? 'password is incorrect'.tr : null,
+                ),
+              ),
+              const SizedBox(height: 10.0),
               TextField(
                 controller: passwordController,
                 obscureText: true,
